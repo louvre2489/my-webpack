a22f87a6b23dd6bcb8dc6896103ea599a76e1d6a
louvre2489 <louvre2489@gmail.com>
2018-08-19 00:41:56 +0900

    use stage-2

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options:{
-            presets:['react']
+            presets:['react', 'stage-2']
           }
         }
       }
